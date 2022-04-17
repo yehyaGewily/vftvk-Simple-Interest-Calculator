052f1781998e291d06eef19d0c8461ade7fdebe3
yehyaGewily <103772686+yehyaGewily@users.noreply.github.com>
2022-04-17 06:54:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if(principal<=0)
+    {
+        alert(' Enter a positive number');
+        break;
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML=
+    `If you deposit ${principal},<br>at an interest rate of ${rate}%<br>You will receive an amount of ${amount},<br>in the year ${year}<br>`;
 }
         
 function updateRate() 
