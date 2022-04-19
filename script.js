184e23c91dc4dc9dd2ccc1ac24e2292aabf4bc02
yehyaGewily <103772686+yehyaGewily@users.noreply.github.com>
2022-04-19 08:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,5 +31,5 @@
    function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
