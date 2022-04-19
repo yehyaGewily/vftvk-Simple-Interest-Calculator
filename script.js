612026063ec7dc756a43bd8a70b1e5c7286144bd
yehyaGewily <103772686+yehyaGewily@users.noreply.github.com>
2022-04-19 07:22:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-function compute()
-{
+ function compute()
+{   //To get the values and to calculate
     var principal = document.getElementById("principal").value;
-    if(principal == undefined || principal <= 0)
-    {
-        window.alert(' Enter a positive number');
-        document.getElementById("principal").focus();
-        return
-        
-    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    window.document.getElementById("result").innerHTML=
-    `If you deposit ${principal},<br>at an interest rate of ${rate}%<br>You will receive an amount of ${amount},<br>in the year ${year}<br>`;
-}
+    
+    //To calculate the interest
+    var interest = principal*years* rate/100;
+
+    var year =new Date().getFullYear()+parseInt(years);
+
+    if(principal == undefined || principal <= 0){
+        alert("Enter a Positive Number");
+        document.getElementById("principal").focus();
         
-function updateRate() 
+        return
+    }else{
+        // Display the text with the calculated values
+        var inr = "If you deposit <mark>" + principal +",</mark><br>";
+        var ra = "at an interest rate of <mark>"+ rate +" %,</mark><br>";
+        var am="You will receive an amount of <mark>" +interest + ",</mark><br>"
+        var ye ="in the year <mark>"+year +"</mark>";
+
+        document.getElementById("result").innerHTML=inr+ra+am+ye;
+
+    }
+    
+
+   }
+   //To update the Rate value
+   function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
